cbd83415cdb6f5aa533c0adb5e4e4afb09be699e
Dilara Gueler <D.Gueler@intershop.de>
2023-01-18 14:37:18 +0100

    hud progress bar style, avatars related to name

--- frontend/iap-module-dev-gamification/src/app/shared/components/hud/hud.component.ts
+++ frontend/iap-module-dev-gamification/src/app/shared/components/hud/hud.component.ts
@@ -1,6 +1,8 @@
+import { LowerCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CurrentUserService, User } from '@intershop/iap-core';
 import { Observable } from 'rxjs';
+import { assetUrl } from 'src/single-spa/asset-url';
 
 @Component({
   selector: 'iap-hud',
@@ -14,4 +16,18 @@ export class HudComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  findAvatar() {
+    var name = '';
+    this.user$.subscribe((currentUser) => {
+      name = currentUser.profile.firstName.toLowerCase();
+    });
+    return name;
+  }
+
+  getUrl() {
+    var firstName = this.findAvatar();
+    var url = `./avatars/${firstName}_avatar.png`;
+    return assetUrl(url);
+  }
 }
