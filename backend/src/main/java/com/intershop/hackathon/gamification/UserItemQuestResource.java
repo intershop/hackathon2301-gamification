e1039b93603fca38db81c27b7a2a22df620370eb
Ronny Wilms <r.wilms@intershop.de>
2023-01-19 12:58:07 +0100

    hack: use GET for claiming quest

--- backend/src/main/java/com/intershop/hackathon/gamification/UserItemQuestResource.java
+++ backend/src/main/java/com/intershop/hackathon/gamification/UserItemQuestResource.java
@@ -62,4 +62,38 @@ public class UserItemQuestResource
 
         return Response.status(400).entity(new Error("internal_error", "An error occurred while processing the data")).build();
     }
+
+    @GET
+    @Produces(RestConstants.MEDIA_TYPE_JSON_API)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response postUser(@PathParam("username") String username, @QueryParam("workitemid") String workItemId)
+    {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (!userOptional.isPresent())
+        {
+            return Response.status(404).entity(new Error("user_not_found", "User '" + username + "' not found")).build();
+        }
+
+        Optional<WorkItem> workItemOptional = ado.getWorkItem(workItemId);
+        if (!workItemOptional.isPresent())
+        {
+            return Response.status(404).entity(new Error("quest_not_found", "Quest '" + workItemId + "' not found")).build();
+        }
+
+        Optional<Quest> questOptional = questRepository.findByIdOptional(String.valueOf(workItemId));
+        if (!questOptional.isPresent())
+        { // should never happen, but who knows ;)
+            questOptional = Optional.of(questRepository.create(questMapper.apply(workItemOptional.get())));
+        }
+
+        /*ExampleBody: {"id": 82713, "fields": {"System.State": "New"}} */
+        Optional<WorkItem> workItemUpdated = ado.updateWorkItem(workItemId, userOptional.get());
+        if (workItemUpdated.isPresent())
+        {
+            return Response.ok(questUpdateMapper.apply(workItemUpdated.get(), questOptional.get())).build();
+        }
+
+        return Response.status(400).entity(new Error("internal_error", "An error occurred while processing the data")).build();
+    }
 }
