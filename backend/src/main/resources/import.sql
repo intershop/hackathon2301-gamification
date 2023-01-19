f092e0d6b398bc60c74fe4b28fe37aea88a1d7db
Can Carlo Arici <can.arici@xim.ag>
2023-01-19 10:55:39 +0100

    changed Achievements descriptions  and achievements to users

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -6,13 +6,13 @@
 
 
 -- init achievements table
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Debugger', 'Solve x Bugs', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Self Sustained', 'Solve x Bugs created by yourself', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Peace Was No Decision', 'Solve x Bugs', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Lucky Luke', 'Solve x Bugs', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'No Hurry', 'Solve x Bugs', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Firefighter', 'Solve x Bugs', 1);
-insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Street', 'Solve x Bugs', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Debugger', 'Bugs fear ME!! Solve x Bugs', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Self Sustained', 'Improvise. Adapt. Overcome. Solve x Bugs created by yourself', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Peace Was No Decision', 'Solve x Bugs, while having more than two open quests ', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Lucky Luke', 'Faster than your own shadow, Solve x Bugs needing less time than your average', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'No Hurry', 'Slow and steady wins the race, Solve x Bugs needing more time than your average', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Firefighter','Throwing yourself in the fire to save the day. Solve x major or higher level Bugs', 1);
+insert into achievement (id, title, description, tier) values (nextval('hibernate_sequence'), 'Street', 'Now I have seen everything! Solve Bugs of each Level, x sets. ', 1);
 
 insert into questUser (	id, username, display_name, email, experience_points) values (nextval('hibernate_sequence'), 'paul_anton_rainer', 'Paul Anton Rainer', 'prainer@intershop.com', 65);
 insert into questUser (	id, username, display_name, email, experience_points) values (nextval('hibernate_sequence'), 'steven_heyder', 'Steven Heyder', 'sheyder@intershop.com',100);
@@ -23,4 +23,24 @@ insert into questUser (	id, username, display_name, email, experience_points) va
 insert into questUser (	id, username, display_name, email, experience_points) values (nextval('hibernate_sequence'), 'dilara_gueler', 'Dilara Güler', 'DGueler@intershop.com', 55);
 insert into questUser (	id, username, display_name, email, experience_points) values (nextval('hibernate_sequence'), 'christian_linde', 'Christian Linde', 'clinde@intershop.com', 169);
 
+insert into user_achievements (user_id , achievement_id) values (8, 1);
+insert into user_achievements (user_id , achievement_id) values (9, 1);
+insert into user_achievements (user_id , achievement_id) values (9, 2);
+insert into user_achievements (user_id , achievement_id) values (9, 4);
+insert into user_achievements (user_id , achievement_id) values (9, 6);
+insert into user_achievements (user_id , achievement_id) values (10, 1);
 insert into user_achievements (user_id , achievement_id) values (10, 2);
+insert into user_achievements (user_id , achievement_id) values (10, 3);
+insert into user_achievements (user_id , achievement_id) values (10, 4);
+insert into user_achievements (user_id , achievement_id) values (10, 6);
+insert into user_achievements (user_id , achievement_id) values (10, 1);
+insert into user_achievements (user_id , achievement_id) values (11, 1);
+insert into user_achievements (user_id , achievement_id) values (12, 1);
+insert into user_achievements (user_id , achievement_id) values (12, 3);
+insert into user_achievements (user_id , achievement_id) values (12, 6);
+insert into user_achievements (user_id , achievement_id) values (13, 1);
+insert into user_achievements (user_id , achievement_id) values (13, 5);
+insert into user_achievements (user_id , achievement_id) values (14, 1);
+insert into user_achievements (user_id , achievement_id) values (15, 1);
+
+
